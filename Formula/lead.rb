a3e335689bef933e9737affc7b1f037d7c6078d0
fattman2008 <mfan.public@gmail.com>
2026-09-21 21:52:24 -0700

    bump lead 0.2.0

--- Formula/lead.rb
+++ Formula/lead.rb
@@ -1,8 +1,8 @@
 class Lead < Formula
   desc "Graphite-shaped stacking with worktree-first parallelism"
   homepage "https://github.com/fattman2008/lead"
-  url "https://github.com/fattman2008/lead/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "7fdf0edcd670d96d26c740862f3bca2ac8c59674f5046714954c4b1d8ce9b69c"
+  url "https://github.com/fattman2008/lead/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "21c9f5af6371441f09d66a1813490a2116bb20ce490fc88006c23310d4515836"
   license "MIT"
 
   depends_on "go" => :build
